9ce69f2e649e1c4eb60a6e651618896e95adaed2
Xiang <denkenli@gmail.com>
2024-02-25 03:26:40 +0100

    e

--- pages/_2024_02_24__activities.py
+++ pages/_2024_02_24__activities.py
@@ -91,6 +91,8 @@ def plot_specific_activity5(df_specific_activity: pd.DataFrame):
         dragmode='pan',  # zoom, pan, select, lasso
     )
     config = {'scrollZoom': True}
+    screen_height=streamlit_js_eval(js_expressions='screen.height', key='get_screen_height_javascript')
+    print(f"screen_heeight: {screen_height}")
     if False:
         screen_height, screen_width = get_screen_height_and_width()
         st.write(f"screen_height: {screen_height}, screen_width: {screen_width}")
@@ -99,8 +101,7 @@ def plot_specific_activity5(df_specific_activity: pd.DataFrame):
 
     st.plotly_chart(fig, use_container_width=True, config=config)
 
-    screen_height=streamlit_js_eval(js_expressions='screen.height', key='get_screen_height_javascript')
-    print(f"screen_heeight: {screen_height}")
+
 
 
 
