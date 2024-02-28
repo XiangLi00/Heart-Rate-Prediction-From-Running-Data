e9c35a8f4e726053566df4bf55c5c1f68f787fe0
Xiang <denkenli@gmail.com>
2024-02-28 23:56:19 +0100

    e

--- utils/helper_process_summary_dfs.py
+++ utils/helper_process_summary_dfs.py
@@ -15,7 +15,12 @@ def process_df_weeks_summary(df_raw: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: The processed DataFrame.
     """
 
+    # Drop columns
     set_cols_to_be_dropped = {'calories_goal', 'calories_consumed_avg', 'spo2_avg', 'spo2_min', 'hydration_goal', 'hydration_avg', 'hydration_intake', 'weight_avg', 'weight_min', 'weight_max'}
     df = df_raw.drop(columns=set_cols_to_be_dropped)
+
+    # Convert column format from datetime to date 
+    df['first_day'] = pd.to_datetime(df['first_day']).dt.date
+
 
     return df
