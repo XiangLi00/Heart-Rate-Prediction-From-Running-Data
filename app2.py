3bb532e7c32bfe2d0e1cbe807c7b9e83aca1480f
Xiang <denkenli@gmail.com>
2024-03-10 18:08:34 +0100

    e

--- app2.py
+++ app2.py
@@ -19,8 +19,20 @@ import streamlit as st
 # print(f"Project root folder: {os.getcwd()}")
 sys.path.append(os.getcwd())  
 
-action = 2
+action = 3
+
 if action == 1:
+    """
+    – Prints a specific function in the.py page 
+    """
+    from streamlit_pages._2024_02_28__weeks_summary import page3
+    #from _2024_02_28__debug_streamlit_community import page3
+    page3()
+
+if action == 2:
+    """
+    – Prints all of the .py page 
+    """
     # Choose page to display
     module_name = [
         '_2024_02_28__weeks_summary',
@@ -33,10 +45,20 @@ if action == 1:
 
     # Shows the content of this streamlit page (just by importing it)
     module = importlib.import_module("streamlit_pages." + module_name) # fancy way of writing "import streamlit_pages._2024_02_28__weeks_summary"
-if action == 2:
-    from streamlit_pages._2024_02_28__weeks_summary import page3
-    #from _2024_02_28__debug_streamlit_community import page3
-    page3()
+
+if action == 3:
+    """
+        Dropdown menu to select the page to display
+    """
+    folder_path = "streamlit_pages"
+    # Retrieve all .py files in folder
+    file_names_of_streamlit_pages_with_py_extension = os.listdir(folder_path)
+    # Remove extionsion. E.g. now get ['_2024_02_24__11_28_monitoring_hr_different_plotting_libs', '_2024_02_24__activities', '_2024_02_28__weeks_summary']
+    file_names_of_streamlit_pages = [os.path.splitext(filename)[0] for filename in filenames if filename.endswith(".py")]
+
+    streamlit_page_displayed = st.selectbox("Select page to display", file_names_of_streamlit_pages)
+
+    importlib.import_module("streamlit_pages." + streamlit_page_displayed) 
 
 
 #st.write("app2.py: v4")
