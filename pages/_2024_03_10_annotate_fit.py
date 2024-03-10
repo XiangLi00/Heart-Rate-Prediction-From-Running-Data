185320c2bfba16304e00ebf4cc78df8d57e44551
Xiang <denkenli@gmail.com>
2024-03-10 23:35:08 +0100

    a

--- pages/_2024_03_10_annotate_fit.py
+++ pages/_2024_03_10_annotate_fit.py
@@ -22,28 +22,18 @@ project_path = os.getcwd()
 
 #sys.path.append(os.path.join(project_path, 'streamlit_pages'))  
 
-from streamlit_pages._2024_03_10_annotate_fit_helper import test1
+# from streamlit_pages._2024_03_10_annotate_fit_helper import test1
+from streamlit_pages import _2024_03_10_annotate_fit_helper
 from utils import helper_load_fit_file, helper_load_specific_df, helper_pandas, helper_streamlit
 # from utils.helper_load_df import load_df_v2, print_column_info_of_all_tables, get_column_info_of_specific_table, generate_report
 
 
+# Section: show and filter running activities
+df_activities, df_running_activities, df_running_activities_filtered = _2024_03_10_annotate_fit_helper.section_running_activities_show_and_filter(project_path=project_path)
 
+# Section: Select specific running activity and show tabular information
 
-st.header("List of running activities")
-# Load the overview of all activities
-df_activities = helper_load_specific_df.load_df_activities(root_path_db=os.path.join(project_path, 'data'))
-
-# Filter only running activities
-# Reason: Add this because otherwise helper_load_fit_file.load_fit_file() otherwise expects columns that don't exist (e.g. {'activity_type', 'power', 'step_length'} for walking/hiking activities and  ('cadence', 'fractional_cadence'), additionally for other activities)
-df_running_activities = df_activities.query('sport == "running"')  
-
-# Add filtering UI
-with st.expander("Search all activities"):
-    df_running_activities_filtered = helper_streamlit.add_df_activities_filtering_ui(df_running_activities)
-    
-    st.write(f'Showing {len(df_running_activities_filtered)} out of {len(df_activities)} running activities')
-    st.dataframe(df_running_activities_filtered)
-
+# df = _2024_03_10_annotate_fit_helper.section_select_activity_and_retrieve_df(df_activities)
 
 st.header("View specific activity")
 # Select specific activity
@@ -64,46 +54,46 @@ df = helper_load_fit_file.load_fit_file(path_fit_file)
 # View df
 st.dataframe(df)
 
-test1(df)
+_2024_03_10_annotate_fit_helper.test1(df)
 
 # ––––––––––––––– Start Plotting
+if False:
+    fig = make_subplots(rows=2, cols=1, 
+                        shared_xaxes=True, shared_yaxes=False, 
+                        specs=[[{"secondary_y": True}], [{"secondary_y": True}],],
+                        vertical_spacing=0.03
+                        )
 
-fig = make_subplots(rows=2, cols=1, 
-                    shared_xaxes=True, shared_yaxes=False, 
-                    specs=[[{"secondary_y": True}], [{"secondary_y": True}],],
-                    vertical_spacing=0.03
-                    )
+    # Add HR trace
+    fig.add_trace(go.Scatter(x=df["timestamp"], y=df["hr"], mode='lines', name='HR', line=dict(color='crimson')), 
+                row=1, col=1, secondary_y=False)
+    # Add Pace trace
+    fig.add_trace(go.Scatter(x=df["timestamp"], y=df["pace"], mode='lines', name='Pace', line=dict(color='deepskyblue')), 
+                row=1, col=1, secondary_y=True)
+    # Add Altitude trace
+    fig.add_trace(go.Scatter(x=df["timestamp"], y=df["altitude"], mode='lines', name='Altitude', line=dict(color='green')),                 row=2, col=1, secondary_y=False)
+    # Add Cadence trace to the same subplot as Altitude
+    fig.add_trace(go.Scatter(x=df["timestamp"], y=df["real_cadence"], mode='lines', name='Cadence'), 
+                row=2, col=1, secondary_y=True)
 
-# Add HR trace
-fig.add_trace(go.Scatter(x=df_specific_activity["timestamp"], y=df_specific_activity["hr"], mode='lines', name='HR', line=dict(color='crimson')), 
-            row=1, col=1, secondary_y=False)
-# Add Pace trace
-fig.add_trace(go.Scatter(x=df_specific_activity["timestamp"], y=df_specific_activity["pace"], mode='lines', name='Pace', line=dict(color='deepskyblue')), 
-            row=1, col=1, secondary_y=True)
-# Add Altitude trace
-fig.add_trace(go.Scatter(x=df_specific_activity["timestamp"], y=df_specific_activity["altitude"], mode='lines', name='Altitude', line=dict(color='green')),                 row=2, col=1, secondary_y=False)
-# Add Cadence trace to the same subplot as Altitude
-fig.add_trace(go.Scatter(x=df_specific_activity["timestamp"], y=df_specific_activity["real_cadence"], mode='lines', name='Cadence'), 
-            row=2, col=1, secondary_y=True)
+    # Update layout settings
+    fig.update_layout(
+        dragmode='pan',  # zoom, pan, select, lasso
+        hovermode='x unified'  # Enable unified hover mode across all traces
+    )
+    fig = update_screen_height_of_fig(fig)
 
-# Update layout settings
-fig.update_layout(
-    dragmode='pan',  # zoom, pan, select, lasso
-    hovermode='x unified'  # Enable unified hover mode across all traces
-)
-fig = update_screen_height_of_fig(fig)
+    # Set y-axis titles
+    fig.update_yaxes(title_text="HR", row=1, col=1, secondary_y=False)
+    fig.update_yaxes(range=[math.log10(15), math.log10(3)], title_text="Pace",  type="log", row=1, col=1,secondary_y=True) # , type="log",autorange="reversed",  ,autorange="reversed"
+    fig.update_yaxes(title_text="Altitude", secondary_y=False, row=2, col=1)
+    fig.update_yaxes(range=[150, 200], title_text="Cadence", secondary_y=True, row=2, col=1)
+    fig.update_yaxes(fixedrange=True)
 
-# Set y-axis titles
-fig.update_yaxes(title_text="HR", row=1, col=1, secondary_y=False)
-fig.update_yaxes(range=[math.log10(15), math.log10(3)], title_text="Pace",  type="log", row=1, col=1,secondary_y=True) # , type="log",autorange="reversed",  ,autorange="reversed"
-fig.update_yaxes(title_text="Altitude", secondary_y=False, row=2, col=1)
-fig.update_yaxes(range=[150, 200], title_text="Cadence", secondary_y=True, row=2, col=1)
-fig.update_yaxes(fixedrange=True)
+    # Your existing code to configure and display the figure
+    config = {'scrollZoom': True}
 
-# Your existing code to configure and display the figure
-config = {'scrollZoom': True}
-
-st.plotly_chart(fig, use_container_width=True, config=config)
+    st.plotly_chart(fig, use_container_width=True, config=config)
 
 
 
--- streamlit_pages/_2024_03_10_annotate_fit_helper.py
+++ streamlit_pages/_2024_03_10_annotate_fit_helper.py
@@ -19,10 +19,30 @@ from streamlit_js_eval import streamlit_js_eval
 
 from utils import helper_load_fit_file, helper_load_specific_df, helper_pandas, helper_streamlit
 # from utils.helper_load_df import load_df_v2, print_column_info_of_all_tables, get_column_info_of_specific_table, generate_report
-project_path = os.getcwd()
+
+st.write(os.getcwd())
+
+
+def section_running_activities_show_and_filter(project_path: str):
+    st.header("List of running activities")
+    # Load the overview of all activities
+    df_activities = helper_load_specific_df.load_df_activities(root_path_db=os.path.join(project_path, 'data'))
+
+    # Filter only running activities
+    # Reason: Add this because otherwise helper_load_fit_file.load_fit_file() otherwise expects columns that don't exist (e.g. {'activity_type', 'power', 'step_length'} for walking/hiking activities and  ('cadence', 'fractional_cadence'), additionally for other activities)
+    df_running_activities = df_activities.query('sport == "running"')  
+
+    # Add filtering UI
+    with st.expander("Search all activities"):
+        df_running_activities_filtered = helper_streamlit.add_df_activities_filtering_ui(df_running_activities)
+        
+        st.write(f'Showing {len(df_running_activities_filtered)} out of {len(df_running_activities)} running activities')
+        st.dataframe(df_running_activities_filtered)
+    
+    return df_activities, df_running_activities, df_running_activities_filtered 
 
 
 def test1(df: pd.DataFrame):
-    st.write("streamlit_pages.annotate_fit_helper.test1() v3")
+    st.write("streamlit_pages.annotate_fit_helper.test1() v4")
 
 
