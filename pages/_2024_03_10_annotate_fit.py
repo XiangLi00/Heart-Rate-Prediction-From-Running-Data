0c2f5d0d16a7dff07e709891f2f9023b9ad20621
Xiang <denkenli@gmail.com>
2024-03-10 22:56:38 +0100

    e

--- pages/_2024_03_10_annotate_fit.py
+++ pages/_2024_03_10_annotate_fit.py
@@ -21,15 +21,20 @@ from utils import helper_load_fit_file, helper_load_specific_df, helper_pandas, 
 # from utils.helper_load_df import load_df_v2, print_column_info_of_all_tables, get_column_info_of_specific_table, generate_report
 project_path = os.getcwd()
 
-st.header("List of activities")
+st.header("List of running activities")
 # Load the overview of all activities
 df_activities = helper_load_specific_df.load_df_activities(root_path_db=os.path.join(project_path, 'data'))
+
+# Filter only running activities
+# Reason: Add this because otherwise helper_load_fit_file.load_fit_file() otherwise expects columns that don't exist (e.g. {'activity_type', 'power', 'step_length'} for walking/hiking activities and  ('cadence', 'fractional_cadence'), additionally for other activities)
+df_running_activities = df_activities.query('sport == "running"')  
+
 # Add filtering UI
 with st.expander("Search all activities"):
-    df_activities_filtered = helper_streamlit.add_df_activities_filtering_ui(df_activities)
+    df_running_activities_filtered = helper_streamlit.add_df_activities_filtering_ui(df_running_activities)
     
-    st.write(f'Showing {len(df_activities_filtered)} out of {len(df_activities)} activities')
-    st.dataframe(df_activities_filtered)
+    st.write(f'Showing {len(df_running_activities_filtered)} out of {len(df_activities)} running activities')
+    st.dataframe(df_running_activities_filtered)
 
 
 st.header("View specific activity")
